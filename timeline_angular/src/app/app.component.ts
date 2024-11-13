a88c3347799b4f7e3c452558677ecabe94e7254c
kanthakdominik <kanthakdominik@gmail.com>
2024-11-13 19:00:14 +0100

    wip

--- timeline_angular/src/app/app.component.ts
+++ timeline_angular/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { MainBarComponent } from './bars/main-bar/main-bar.component';
 import { SettingsBarComponent } from "./bars/settings-bar/settings-bar.component";
 import { CategoriesBarComponent } from "./bars/categories-bar/categories-bar.component";
@@ -8,7 +7,7 @@ import { EventsCardsComponent } from "./events-cards/events-cards.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainBarComponent, SettingsBarComponent, CategoriesBarComponent, EventsCardsComponent],
+  imports: [MainBarComponent, SettingsBarComponent, CategoriesBarComponent, EventsCardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
