21d01a7516ded0be21cc88086eaa11c399bd70ba
kanthakdominik <kanthakdominik@gmail.com>
2024-11-20 00:36:13 +0100

    update server

--- timeline_angular/server.js
+++ timeline_angular/server.js
@@ -2,13 +2,31 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist/timeline-angular')));
+const appDirectory = process.cwd();
+console.log('Application directory:', appDirectory);
 
+const distPath = path.join(appDirectory, 'dist/timeline_angular/browser');
+console.log('Dist path:', distPath);
+
+console.log('Directory contents:', require('fs').readdirSync(appDirectory));
+if (require('fs').existsSync(path.join(appDirectory, 'dist'))) {
+    console.log('Dist contents:', require('fs').readdirSync(path.join(appDirectory, 'dist')));
+}
+
+app.use(express.static(distPath));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dist/timeline-angular/index.html'));
+    const indexPath = path.join(distPath, 'index.html');
+    console.log('Trying to serve:', indexPath);
+    
+    if (require('fs').existsSync(indexPath)) {
+        res.sendFile(indexPath);
+    } else {
+        res.status(404).send('index.html not found. Path: ' + indexPath);
+    }
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+    console.log('Server is running on port', port);
+    console.log('Serving files from:', distPath);
 });
